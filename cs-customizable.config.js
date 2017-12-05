3c98635f176b7f64924b2d4daf69d45759ecae14
psxcode <psxcode@gmail.com>
2017-12-05 20:36:25 +0100

    chore: initial commit

---
+++ cs-customizable.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+  types: [
+    {value: 'feat',     name: 'feat:     A new feature'},
+    {value: 'fix',      name: 'fix:      A bug fix'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
+    {value: 'test',     name: 'test:     Adding missing tests'},
+    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
+    {value: 'WIP',      name: 'WIP:      Work in progress'}
+  ],
+
+  scopes: [
+    {name: 'doge'},
+  ],
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+    ]
+  },
+
+  // override the messages, defaults are as follows
+  messages: {
+    type: 'Select the type of change that you\'re committing:',
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?'
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix']
+};
