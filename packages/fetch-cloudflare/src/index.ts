4c3fd234e104b15694ccde34135018e8b14c83b3
alex.feinstein <alex.feinstein@klarna.com>
2017-12-15 15:12:14 +0100

    refactor(fetch-cloudflare): simplify higher-order fetches

--- packages/fetch-cloudflare/src/index.ts
+++ packages/fetch-cloudflare/src/index.ts
@@ -1,44 +1,46 @@
-import { default as fetch, RequestInit, Response } from 'node-fetch'
-import { CookieJar } from 'tough-cookie'
-import { promisify } from './helpers'
+import { default as fetch, Request, RequestInit, Response } from 'node-fetch'
 
-export const getFetch = (request: typeof fetch) =>
-  (url: string, opts: RequestInit = {}): Promise<Response> =>
-    request(url, { ...opts, method: 'GET' })
+export type FetchFn = typeof fetch
 
-export const apisignFetch = (hash: (url: string) => string) => (request: typeof fetch) =>
-  (url: string, opts: RequestInit = {}): Promise<Response> => {
-    const { headers = {} } = opts
-    return request(url, {
-      ...opts,
-      headers: {
-        ...headers,
-        apisign: hash(url)
-      }
-    })
-  }
+export const fetchGet =
+  (request: FetchFn): FetchFn =>
+    (url: string, opts: RequestInit = {}): Promise<Response> =>
+      request(url, { ...opts, method: 'GET' })
 
-export const cookiesFetch = (jar: CookieJar) => (request: typeof fetch) => {
-  const setCookie: (cookie: string, url: string) => Promise<void> = promisify(jar.setCookie.bind(jar))
-  const getCookie: (url: string) => Promise<string> = promisify(jar.getCookieString.bind(jar))
-  return (url: string, opts: RequestInit = {}): Promise<Response> =>
-    getCookie(url)
-      .then((cookie: string): Promise<Response> => {
-        const { headers = {} } = opts
-        return request(url, {
-          ...opts,
-          headers: {
-            ...headers,
-            ...cookie ? { cookie } : {}
-          }
+export const fetchApisign = (hash: (url: string) => string) =>
+  (request: FetchFn): FetchFn =>
+    (url: string, opts: RequestInit = {}): Promise<Response> => {
+      const { headers = {} } = opts
+      return request(url, {
+        ...opts,
+        headers: {
+          ...headers,
+          apisign: hash(url)
+        }
+      })
+    }
+
+export type GetCookieFn = (url: string) => Promise<string>
+export type SetCookieFn = (cookie: string, url: string) => Promise<void>
+
+export const fetchCookies = (getCookie: GetCookieFn, setCookie: SetCookieFn) =>
+  (request: FetchFn): FetchFn =>
+    (url: string, opts: RequestInit = {}): Promise<Response> =>
+      getCookie(url)
+        .then((cookie: string) => {
+          const { headers = {} } = opts
+          return request(url, {
+            ...opts,
+            headers: {
+              ...headers,
+              ...cookie ? { cookie } : {}
+            }
+          })
         })
-      })
-      .then((resp: Response) => {
-        const { url, headers } = resp
-        const cookieStr: string = headers.get('set-cookie') || ''
-        const cookies: string[] = cookieStr ? cookieStr.split(',') : []
-        return Promise
-          .all(cookies.map((c: string) => setCookie(c, url)))
-          .then(() => resp)
-      })
-}
+        .then((resp: Response) => {
+          const { url, headers } = resp
+          const cookieStr = headers.get('set-cookie') || ''
+          return cookieStr
+            ? setCookie(cookieStr, url).then(() => resp)
+            : resp
+        })
