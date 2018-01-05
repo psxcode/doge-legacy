b9c093a6dea89e8e22ec46a1070a49fb498a0f65
alex.feinstein <alex.feinstein@klarna.com>
2018-01-05 18:07:26 +0100

    refactor(rx-node): add unsubscribe

--- packages/rx-node/src/observable/from-stream.ts
+++ packages/rx-node/src/observable/from-stream.ts
@@ -3,24 +3,31 @@ import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/share'
 
 export function fromStream<T extends string | Buffer> (source: ReadableStream): Observable<T> {
-  return new Observable<T>((observer) => {
-    const onData = (chunk: T) => {
-      observer.next(chunk)
-    }
-    const onEnd = () => {
-      source.removeListener('data', onData)
-      source.removeListener('end', onEnd)
-      source.removeListener('error', onError)
-      observer.complete()
-    }
-    const onError = (e: any) => {
-      source.removeListener('data', onData)
-      source.removeListener('end', onEnd)
-      source.removeListener('error', onError)
-      observer.error(e)
-    }
+  const subscribeToStream = (onData: (data?: T) => void,
+                             onError: (e: any) => void,
+                             onEnd: () => void) => {
     source.on('data', onData)
     source.on('end', onEnd)
     source.on('error', onError)
+    return () => {
+      source.removeListener('data', onData)
+      source.removeListener('end', onEnd)
+      source.removeListener('error', onError)
+    }
+  }
+  return new Observable<T>((observer) => {
+    const unsub = subscribeToStream(
+      (chunk: T) => {
+        observer.next(chunk)
+      },
+      (e: any) => {
+        unsub && unsub()
+        observer.error(e)
+      },
+      () => {
+        unsub && unsub()
+        observer.complete()
+      })
+    return unsub
   }).share()
 }
