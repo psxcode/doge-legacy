cee886ef69bb0eb6fda221c80fab9850e99ab958
alex.feinstein <alex.feinstein@klarna.com>
2018-01-19 18:17:39 +0100

    refactor(stream-test): add expectation to writable test

--- packages/stream-test/src/writable.spec.ts
+++ packages/stream-test/src/writable.spec.ts
@@ -3,16 +3,24 @@ import {
   makeMediumStrings,
   makeSmallStrings,
   makeWritableProducer,
-  makeWritableTest,
+  makeWritableTest, SpyFn,
   xmakeWritableTest
 } from './test-helpers'
 import { makeWritable } from './writable'
+
+const expectSameData = <T> (data: T[], spy: SpyFn<T>) => {
+  expect(spy.data().join('')).deep.eq(data.join(''))
+}
+const expectSameCallCount = <T> (data: T[], spy: SpyFn<T>) => {
+  expect(spy.data()).deep.eq(data)
+  expect(spy.callCount()).eq(data.length)
+}
 
 describe('', function () {
   this.slow(1000)
 
   makeWritableTest(makeMediumStrings(),
-    (spy) => makeWritable({ writeDelayMs: undefined })(spy, { highWaterMark: 256 }),
-    (stream, data) => makeWritableProducer(stream, data, { eager: false })
-    )
+    (spy) => makeWritable({ writeDelayMs: 10 })(spy, { highWaterMark: 256 }),
+    (stream, data) => makeWritableProducer(stream, data, { eager: true }),
+    expectSameCallCount)
 })
