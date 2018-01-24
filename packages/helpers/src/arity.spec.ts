1c1337f5bddb8e4d50dd0ba3bc110c0b41189d2f
alex.feinstein <alex.feinstein@klarna.com>
2018-01-24 10:07:49 +0100

    refactor(helpers): add more descriptions

--- packages/helpers/src/arity.spec.ts
+++ packages/helpers/src/arity.spec.ts
@@ -4,42 +4,44 @@ import { binary, nullary, ternary, unary } from './arity'
 
 const makeSpy = () => sinon.spy()
 
-describe('[ arity / nullary ]', function () {
-  it('should work', function () {
-    const spy = makeSpy()
-    const un = nullary(spy)
-    un('a', 'b', 'c')
-    sinon.assert.calledOnce(spy)
-    sinon.assert.calledWithExactly(spy)
+describe('[ arity ]', function () {
+  describe('[ nullary ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const un = nullary(spy)
+      un('a', 'b', 'c')
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy)
+    })
   })
-})
 
-describe('[ arity / unary ]', function () {
-  it('should work', function () {
-    const spy = makeSpy()
-    const un = unary(spy)
-    un('a', 'b', 'c')
-    sinon.assert.calledOnce(spy)
-    sinon.assert.calledWithExactly(spy, 'a')
+  describe('[ unary ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const un = unary(spy)
+      un('a', 'b', 'c')
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, 'a')
+    })
   })
-})
 
-describe('[ arity / binary ]', function () {
-  it('should work', function () {
-    const spy = makeSpy()
-    const un = binary(spy)
-    un('a', 'b', 'c')
-    sinon.assert.calledOnce(spy)
-    sinon.assert.calledWithExactly(spy, 'a', 'b')
+  describe('[ binary ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const un = binary(spy)
+      un('a', 'b', 'c')
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, 'a', 'b')
+    })
   })
-})
 
-describe('[ arity / ternary ]', function () {
-  it('should work', function () {
-    const spy = makeSpy()
-    const un = ternary(spy)
-    un('a', 'b', 'c', 'd')
-    sinon.assert.calledOnce(spy)
-    sinon.assert.calledWithExactly(spy, 'a', 'b', 'c')
+  describe('[ ternary ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const un = ternary(spy)
+      un('a', 'b', 'c', 'd')
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, 'a', 'b', 'c')
+    })
   })
 })
--- packages/helpers/src/memoize.spec.ts
+++ packages/helpers/src/memoize.spec.ts
@@ -8,11 +8,17 @@ const identitySerializer = (value: any) => value
 const makeMapCache = (): ICache<any, any> => new Map()
 const cacheFactories = [makeObjectCache, makeMapCache]
 
-const makeSpyCache = (cache: {[k: string]: any} = {}): ICache<any, any> => {
+const makeSpyCache = (cache: { [k: string]: any } = {}): ICache<any, any> => {
   const c = {
-    get (key: any) { return cache[key] },
-    set (key: any, value: any) { cache[key] = value },
-    has (key: any) { return Object.prototype.hasOwnProperty.call(cache, key) }
+    get (key: any) {
+      return cache[key]
+    },
+    set (key: any, value: any) {
+      cache[key] = value
+    },
+    has (key: any) {
+      return Object.prototype.hasOwnProperty.call(cache, key)
+    }
   }
   sinon.stub(c, 'get').callThrough()
   sinon.stub(c, 'set').callThrough()
@@ -20,85 +26,88 @@ const makeSpyCache = (cache: {[k: string]: any} = {}): ICache<any, any> => {
   return c
 }
 
-cacheFactories.forEach((makeCache: () => ICache<any, any>) => {
-  describe(`[ memoize / Cache ]`, function () {
-    describe('[ get ]', function () {
-      it('should return undefined', function () {
-        const cache = makeCache()
-        expect(cache.get('key')).undefined
-      })
-
-      it('should return proper value', function () {
-        const cache = makeCache()
-        cache.set('key', 'value')
-        expect(cache.get('key')).eq('value')
-      })
-    })
-
-    describe('[ has ]', function () {
-      it('should return false', function () {
-        const cache = makeCache()
-        expect(cache.has('key')).eq(false)
-      })
-
-      it('should return true', function () {
-        const cache = makeCache()
-        cache.set('key', 'value')
-        expect(cache.has('key')).eq(true)
-      })
-    })
-
-    describe('[ set ]', function () {
-      it('should work', function () {
-        const cache = makeCache()
-        cache.set('key', 'value')
-        expect(cache.get('key')).eq('value')
-      })
-
-      it('should replace', function () {
-        const cache = makeCache()
-        cache.set('key', 'value')
-        cache.set('key', 'other value')
-        expect(cache.get('key')).eq('other value')
-      })
-    })
-  })
-})
-
 describe('[ memoize ]', function () {
-  it('should set value to cache', function () {
-    const cache = makeSpyCache()
-    const spy = sinon.mock().returns('value')
-    const serializerSpy = sinon.mock().returns('key')
-    const memo = memoize(cache, serializerSpy)(spy)
 
-    const res = memo('arg')
+  cacheFactories.forEach((makeCache: () => ICache<any, any>) => {
+    describe(`[ cache ]`, function () {
+      describe('[ get ]', function () {
+        it('should return undefined', function () {
+          const cache = makeCache()
+          expect(cache.get('key')).undefined
+        })
 
-    expect(res).eq('value')
-    sinon.assert.calledOnce(serializerSpy)
-    expect(serializerSpy.getCall(0).args).deep.eq(['arg'])
-    sinon.assert.calledOnce(cache.has as sinon.SinonSpy)
-    expect((cache.has as sinon.SinonSpy).getCall(0).args).deep.eq(['key'])
-    sinon.assert.calledOnce(cache.set as sinon.SinonSpy)
-    expect((cache.set as sinon.SinonSpy).getCall(0).args).deep.eq(['key', 'value'])
-    sinon.assert.notCalled(cache.get as sinon.SinonSpy)
+        it('should return proper value', function () {
+          const cache = makeCache()
+          cache.set('key', 'value')
+          expect(cache.get('key')).eq('value')
+        })
+      })
+
+      describe('[ has ]', function () {
+        it('should return false', function () {
+          const cache = makeCache()
+          expect(cache.has('key')).eq(false)
+        })
+
+        it('should return true', function () {
+          const cache = makeCache()
+          cache.set('key', 'value')
+          expect(cache.has('key')).eq(true)
+        })
+      })
+
+      describe('[ set ]', function () {
+        it('should work', function () {
+          const cache = makeCache()
+          cache.set('key', 'value')
+          expect(cache.get('key')).eq('value')
+        })
+
+        it('should replace', function () {
+          const cache = makeCache()
+          cache.set('key', 'value')
+          cache.set('key', 'other value')
+          expect(cache.get('key')).eq('other value')
+        })
+      })
+    })
   })
 
-  it('should get value from cache', function () {
-    const cache = makeSpyCache({ 'key': 'value' })
-    const spy = sinon.mock().returns('value')
-    const serializerSpy = sinon.mock().returns('key')
-    const memo = memoize(cache, serializerSpy)(spy)
+  describe('[ memoize ]', function () {
+    it('should set value to cache', function () {
+      const cache = makeSpyCache()
+      const spy = sinon.mock().returns('value')
+      const serializerSpy = sinon.mock().returns('key')
+      const memo = memoize(cache, serializerSpy)(spy)
 
-    const res = memo('arg')
+      const res = memo('arg')
 
-    expect(res).eq('value')
-    sinon.assert.calledOnce(serializerSpy)
-    expect(serializerSpy.getCall(0).args).deep.eq(['arg'])
-    sinon.assert.calledOnce(cache.has as sinon.SinonSpy)
-    expect((cache.has as sinon.SinonSpy).getCall(0).args).deep.eq(['key'])
-    sinon.assert.notCalled(cache.set as sinon.SinonSpy)
-    sinon.assert.calledOnce(cache.get as sinon.SinonSpy)
-    expect((cache.get as sinon.SinonSpy).getCall(0).args).deep.eq(['key'])
+      expect(res).eq('value')
+      sinon.assert.calledOnce(serializerSpy)
+      expect(serializerSpy.getCall(0).args).deep.eq(['arg'])
+      sinon.assert.calledOnce(cache.has as sinon.SinonSpy)
+      expect((cache.has as sinon.SinonSpy).getCall(0).args).deep.eq(['key'])
+      sinon.assert.calledOnce(cache.set as sinon.SinonSpy)
+      expect((cache.set as sinon.SinonSpy).getCall(0).args).deep.eq(['key', 'value'])
+      sinon.assert.notCalled(cache.get as sinon.SinonSpy)
+    })
+
+    it('should get value from cache', function () {
+      const cache = makeSpyCache({ 'key': 'value' })
+      const spy = sinon.mock().returns('value')
+      const serializerSpy = sinon.mock().returns('key')
+      const memo = memoize(cache, serializerSpy)(spy)
+
+      const res = memo('arg')
+
+      expect(res).eq('value')
+      sinon.assert.calledOnce(serializerSpy)
+      expect(serializerSpy.getCall(0).args).deep.eq(['arg'])
+      sinon.assert.calledOnce(cache.has as sinon.SinonSpy)
+      expect((cache.has as sinon.SinonSpy).getCall(0).args).deep.eq(['key'])
+      sinon.assert.notCalled(cache.set as sinon.SinonSpy)
+      sinon.assert.calledOnce(cache.get as sinon.SinonSpy)
+      expect((cache.get as sinon.SinonSpy).getCall(0).args).deep.eq(['key'])
+    })
   })
 })
