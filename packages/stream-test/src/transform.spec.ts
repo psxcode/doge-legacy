21306b7422d643f031f1d14765cd11538b208e45
alex.feinstein <alex.feinstein@klarna.com>
2018-01-25 18:34:27 +0100

    feat(stream-test): add transform stream utilites

--- packages/stream-test/src/transform.spec.ts
+++ packages/stream-test/src/transform.spec.ts
@@ -3,34 +3,54 @@ import { makeReadable } from './readable'
 import { makeWritable } from './writable'
 import {
   expectSameCallCount,
-  expectSameData, makeSmallRange,
+  expectSameData,
+  makeSmallRange,
   makeSmallStrings,
   makeTransformTest,
   xmakeTransformTest
 } from './test-helpers'
 import { iterate, identity } from '@doge/helpers'
-import { filter, map as transformMap } from './transform'
+import { mapStream, filterStream, reduceStream } from './transform'
 
 const multiply = (multiplier: number) => (value: number) => value * multiplier
+const isEven = (value: number) => value % 2 === 0
+const addAll = (acc = 0, value: number) => acc + value
 
 describe('[ transform ]', function () {
   describe('[ map ]', function () {
-    makeTransformTest<string>(makeSmallStrings(6),
+    xmakeTransformTest<string>(makeSmallStrings(6),
       (data) => makeReadable({})({ encoding: 'utf8' })(iterate(data)),
       (spy) => makeWritable({})({ decodeStrings: false })(spy),
-      () => transformMap({})(identity),
+      () => mapStream({})(identity),
       expectSameCallCount)
 
     const smallNumbers = makeSmallRange(4)
     xmakeTransformTest<number>(smallNumbers,
       (data) => makeReadable({})({ objectMode: true })(iterate(data)),
       (spy) => makeWritable({})({ objectMode: true })(spy),
-      () => transformMap({ objectMode: true })(multiply(2)),
+      () => mapStream({ objectMode: true })(multiply(2)),
       (data, spy) => {
         expect(spy.data()).deep.eq(Array.from(data).map(multiply(2)))
       })
   })
 
   describe('[ filter ]', function () {
+    xmakeTransformTest<number>(makeSmallRange(4),
+      (data) => makeReadable({})({ objectMode: true })(iterate(data)),
+      (spy) => makeWritable({})({ objectMode: true })(spy),
+      () => filterStream({ objectMode: true })(isEven),
+      (data, spy) => {
+        expect(spy.data()).deep.eq(Array.from(data).filter(isEven))
+      })
+  })
+
+  describe('[ reduce ]', function () {
+    makeTransformTest<number>(makeSmallRange(4),
+      (data) => makeReadable({})({ objectMode: true })(iterate(data)),
+      (spy) => makeWritable({})({ objectMode: true })(spy),
+      () => reduceStream({ objectMode: true })(addAll),
+      (data, spy) => {
+        expect(spy.data()).deep.eq([Array.from(data).reduce(addAll)])
+      })
   })
 })
--- packages/stream-test/src/transform.ts
+++ packages/stream-test/src/transform.ts
@@ -1,35 +1,43 @@
 import { Transform, TransformOptions } from 'stream'
 
-export const map = (opts: TransformOptions = {}) => (xf: (value: any) => any) =>
+export type StreamTransformFn = (chunk: any, encoding: string, callback: Function) => void
+export type StreamFlush = (callback: Function) => any
+
+export const mapTransform = (xf: (value: any) => any): StreamTransformFn =>
+  (chunk, encoding, callback) => {
+    let res
+    try {
+      res = xf(chunk)
+    } catch (e) {
+      return callback(e)
+    }
+    callback(null, res)
+  }
+
+export const mapStream = (opts: TransformOptions = {}) => (xf: (value: any) => any) =>
   new Transform({
     ...opts,
-    objectMode: true,
-    transform (chunk, encoding, callback) {
-      let res
-      try {
-        res = xf(chunk)
-      } catch (e) {
-        return callback(e)
-      }
-      callback(null, res)
-    }
+    transform: mapTransform(xf)
   })
 
-export const filter = (opts: TransformOptions = {}) => (predicate: (value: any) => boolean) =>
+export const filterTransform = (predicate: (value: any) => boolean): StreamTransformFn =>
+  (chunk, encoding, callback) => {
+    let res
+    try {
+      res = predicate(chunk)
+    } catch (e) {
+      return callback(e)
+    }
+    callback(null, res ? chunk : undefined)
+  }
+
+export const filterStream = (opts: TransformOptions = {}) => (predicate: (value: any) => boolean) =>
   new Transform({
     ...opts,
-    transform (chunk, encoding, callback) {
-      let res
-      try {
-        res = predicate(chunk)
-      } catch (e) {
-        return callback(e)
-      }
-      callback(null, res ? chunk : undefined)
-    }
+    transform: filterTransform(predicate)
   })
 
-export const reduce = (opts: TransformOptions = {}) => (reducer: (state: any, value: any) => any) => {
+export const reduceStream = (opts: TransformOptions = {}) => (reducer: (state: any, value: any) => any) => {
   let state: any
   return new Transform({
     ...opts,
@@ -37,8 +45,9 @@ export const reduce = (opts: TransformOptions = {}) => (reducer: (state: any, va
       try {
         state = reducer(state, chunk)
       } catch (e) {
-        callback(e)
+        return callback(e)
       }
+      callback()
     },
     flush (callback) {
       callback(null, state)
