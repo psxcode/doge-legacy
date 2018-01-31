8385e2a86a392a28439e3bf75160bcef4395bf5c
alex.feinstein <alex.feinstein@klarna.com>
2018-01-31 16:38:08 +0100

    feat(helpers): add more pipe utils

--- packages/helpers/src/pipe.ts
+++ packages/helpers/src/pipe.ts
@@ -25,3 +25,29 @@ export function pipeAsync<A, B, C, D, E, F, G, H, I> (fn0: AsyncPipeFn<A, B>, fn
 export function pipeAsync (...fns: AsyncPipeFn<any, any>[]): AsyncFn<any, any> {
   return (initial: any) => fns.reduce((arg, fn) => arg.then(fn), Promise.resolve(initial))
 }
+
+export function all<A> (): (arg: A) => [A]
+export function all<A, B> (fn: PipeFn<A, B>): PipeFn<A, [B]>
+export function all<A, B, C> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>): PipeFn<A, [B, C]>
+export function all<A, B, C, D> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>, fn2: PipeFn<A, D>): PipeFn<A, [B, C, D]>
+export function all<A, B, C, D, E> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>, fn2: PipeFn<A, D>, fn3: PipeFn<A, E>): PipeFn<A, [B, C, D, E]>
+export function all<A, B, C, D, E, F> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>, fn2: PipeFn<A, D>, fn3: PipeFn<A, E>, fn4: PipeFn<A, F>): PipeFn<A, [B, C, D, E, F]>
+export function all<A, B, C, D, E, F, G> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>, fn2: PipeFn<A, D>, fn3: PipeFn<A, E>, fn4: PipeFn<A, F>, fn5: PipeFn<A, G>): PipeFn<A, [B, C, D, E, F, G]>
+export function all<A, B, C, D, E, F, G, H> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>, fn2: PipeFn<A, D>, fn3: PipeFn<A, E>, fn4: PipeFn<A, F>, fn5: PipeFn<A, G>, fn6: PipeFn<A, H>): PipeFn<A, [B, C, D, E, F, G, H]>
+export function all<A, B, C, D, E, F, G, H, I> (fn0: PipeFn<A, B>, fn1: PipeFn<A, C>, fn2: PipeFn<A, D>, fn3: PipeFn<A, E>, fn4: PipeFn<A, F>, fn5: PipeFn<A, G>, fn6: PipeFn<A, H>, fn7: PipeFn<A, I>): PipeFn<A, [B, C, D, E, F, G, H, I]>
+export function all (...fns: PipeFn<any, any>[]) {
+  return (value: any) => fns.length ? fns.map(fn => fn(value)) : [value]
+}
+
+export function allAsync<A> (): (arg: A) => Promise<[A]>
+export function allAsync<A, B> (fn: AsyncPipeFn<A, B>): AsyncFn<A, [B]>
+export function allAsync<A, B, C> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>): AsyncFn<A, [B, C]>
+export function allAsync<A, B, C, D> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>, fn2: AsyncPipeFn<A, D>): AsyncFn<A, [B, C, D]>
+export function allAsync<A, B, C, D, E> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>, fn2: AsyncPipeFn<A, D>, fn3: AsyncPipeFn<A, E>): AsyncFn<A, [B, C, D, E]>
+export function allAsync<A, B, C, D, E, F> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>, fn2: AsyncPipeFn<A, D>, fn3: AsyncPipeFn<A, E>, fn4: AsyncPipeFn<A, F>): AsyncFn<A, [B, C, D, E, F]>
+export function allAsync<A, B, C, D, E, F, G> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>, fn2: AsyncPipeFn<A, D>, fn3: AsyncPipeFn<A, E>, fn4: AsyncPipeFn<A, F>, fn5: AsyncPipeFn<A, G>): AsyncFn<A, [B, C, D, E, F, G]>
+export function allAsync<A, B, C, D, E, F, G, H> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>, fn2: AsyncPipeFn<A, D>, fn3: AsyncPipeFn<A, E>, fn4: AsyncPipeFn<A, F>, fn5: AsyncPipeFn<A, G>, fn6: AsyncPipeFn<A, H>): AsyncFn<A, [B, C, D, E, F, G, H]>
+export function allAsync<A, B, C, D, E, F, G, H, I> (fn0: AsyncPipeFn<A, B>, fn1: AsyncPipeFn<A, C>, fn2: AsyncPipeFn<A, D>, fn3: AsyncPipeFn<A, E>, fn4: AsyncPipeFn<A, F>, fn5: AsyncPipeFn<A, G>, fn6: AsyncPipeFn<A, H>, fn7: AsyncPipeFn<A, I>): AsyncFn<A, [B, C, D, E, F, G, H, I]>
+export function allAsync (...fns: AsyncPipeFn<any, any>[]) {
+  return (value: any) => fns.length ? Promise.all(fns.map(fn => Promise.resolve(fn(value)))) : Promise.resolve([value])
+}
--- packages/helpers/src/types.ts
+++ packages/helpers/src/types.ts
@@ -1,3 +1,3 @@
 export type PipeFn<T, R> = (arg?: T) => R
 export type AsyncFn <A, B> = (arg?: A) => Promise<B>
-export type AsyncPipeFn<A, B> = PipeFn<A, B> | AsyncFn<A, B>
+export type AsyncPipeFn<A, B> = (arg?: A) => B | Promise<B>
