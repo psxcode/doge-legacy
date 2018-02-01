55448433c8975415dad96845ed7cafebd2c027fb
alex.feinstein <alex.feinstein@klarna.com>
2018-02-01 18:06:02 +0100

    feat(stream): add combine

---
+++ packages/stream/src/combine.ts
@@ -0,0 +1,34 @@
+import ReadableStream = NodeJS.ReadableStream
+import { Readable, ReadableOptions } from 'stream'
+import { emptyRaw } from './empty'
+import { subscribeEx } from './subscribe'
+import { IEEValue } from './events'
+
+export const combineRaw = (opts: ReadableOptions) => (...streams: ReadableStream[]): ReadableStream => {
+  let unsubscribe: (() => void) | undefined
+  let latest = new Array(streams.length)
+  return streams.length
+    ? new Readable({
+      ...opts,
+      read () {
+        if (!unsubscribe) {
+          unsubscribe = subscribeEx({
+            next: ({ value, index }: IEEValue) => {
+              latest = latest.slice()
+              latest[index] = value
+              this.push(latest)
+            },
+            error: ({ value }: IEEValue) => this.emit('error', value),
+            complete: () => this.push(null)
+          })(...streams)
+        }
+      },
+      destroy () {
+        unsubscribe && unsubscribe()
+        unsubscribe = undefined
+      }
+    })
+    : emptyRaw(opts)()
+}
+
+export const combine = combineRaw({ objectMode: true })
