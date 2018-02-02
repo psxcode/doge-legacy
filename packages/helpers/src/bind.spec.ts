d9f9bd0882b1c661004509d7e6c1f9205a795aa7
alex.feinstein <alex.feinstein@klarna.com>
2018-02-02 14:18:49 +0100

    refactor(helpers): add bindCtx tests

--- packages/helpers/src/bind.spec.ts
+++ packages/helpers/src/bind.spec.ts
@@ -1,24 +1,48 @@
 import { expect } from 'chai'
-import { bind } from './bind'
+import { bind, bindCtx } from './bind'
 import { pipe } from './pipe'
 
 const id = (value: any) => value
 const add = (a: number, b: number) => a + b
 const sum = (...args: number[]) => args.reduce(add)
+const getCtx = function () { return this }
 
 describe('[ bind ]', function () {
-  it('should work as a constant', function () {
-    const binded = bind(10)(id)
-    expect(binded()).eq(10)
+  describe('[ bind ]', function () {
+    it('should work as a constant', function () {
+      const binded = bind(10)(id)
+      expect(binded()).eq(10)
+    })
+
+    it('should work', function () {
+      const binded = bind(10)(add)
+      expect(binded(10)).eq(20)
+    })
+
+    it('should work with pipe', function () {
+      const binded = pipe(bind(10, 20), bind(10), bind(2))(sum)
+      expect(binded()).eq(42)
+    })
   })
 
-  it('should work', function () {
-    const binded = bind(10)(add)
-    expect(binded(10)).eq(20)
-  })
+  describe('[ bindCtx ]', function () {
+    it('should work as a constant', function () {
+      const ctx = {
+        value: 10
+      }
+      const binded = bindCtx(ctx)(getCtx)
+      expect(binded()).deep.eq(ctx)
+    })
 
-  it('should work with pipe', function () {
-    const binded = pipe(bind(10, 20), bind(10), bind(2))(sum)
-    expect(binded()).eq(42)
+    it('should work with pipe', function () {
+      const ctx = {
+        value: 10
+      }
+      const ctx2 = {
+        value: 20
+      }
+      const binded = pipe(bindCtx(ctx), bindCtx(ctx2))(getCtx)
+      expect(binded()).deep.eq(ctx)
+    })
   })
 })
