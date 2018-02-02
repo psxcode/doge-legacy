ed82d7a11aa99cdf832883270d2ca12e6edc932f
alex.feinstein <alex.feinstein@klarna.com>
2018-02-02 14:19:13 +0100

    feat(helpers): add more arity utils

--- packages/helpers/src/arity.spec.ts
+++ packages/helpers/src/arity.spec.ts
@@ -1,7 +1,17 @@
 /* tslint:disable no-empty */
 import { expect } from 'chai'
 import * as sinon from 'sinon'
-import { binary, nullary, ternary, unary, voidify } from './arity'
+import {
+  binary,
+  gather,
+  named,
+  nullary,
+  positional,
+  spread,
+  ternary,
+  unary,
+  voidify
+} from './arity'
 import { identity } from './identity'
 
 const makeSpy = () => sinon.spy()
@@ -51,6 +61,46 @@ describe('[ arity ]', function () {
     it('should work', function () {
       const result = voidify(identity)('value')
       expect(result).eq(undefined)
+    })
+  })
+
+  describe('[ spread ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const f = spread(spy)
+      f(['a', 'b', 'c'])
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, 'a', 'b', 'c')
+    })
+  })
+
+  describe('[ gather ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const f = gather(spy)
+      f('a', 'b', 'c')
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, ['a', 'b', 'c'])
+    })
+  })
+
+  describe('[ named ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const f = named('a', 'b', 'c')(spy)
+      f(1, 2, 3)
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, { a: 1, b: 2, c: 3 })
+    })
+  })
+
+  describe('[ positional ]', function () {
+    it('should work', function () {
+      const spy = makeSpy()
+      const f = positional('a', 'b', 'c')(spy)
+      f({ a: 1, b: 2, c: 3 })
+      sinon.assert.calledOnce(spy)
+      sinon.assert.calledWithExactly(spy, 1, 2, 3)
     })
   })
 })
--- packages/helpers/src/arity.ts
+++ packages/helpers/src/arity.ts
@@ -1,14 +1,29 @@
 export const nullary = <R> (fn: (...args: any[]) => R) =>
-  (...args: any[]): R => fn()
+  (..._: any[]): R => fn()
 
 export const unary = <A, R> (fn: (arg: A, ...args: any[]) => R) =>
-  (arg: A, ...args: any[]): R => fn(arg)
+  (arg: A, ..._: any[]): R => fn(arg)
 
 export const binary = <A, B, R> (fn: (arg1: A, arg2: B, ...args: any[]) => R) =>
-  (arg1: A, arg2: B, ...args: any[]): R => fn(arg1, arg2)
+  (arg1: A, arg2: B, ..._: any[]): R => fn(arg1, arg2)
 
 export const ternary = <A, B, C, R> (fn: (arg1: A, arg2: B, arg3: C, ...args: any[]) => R) =>
-  (arg1: A, arg2: B, arg3: C, ...args: any[]): R => fn(arg1, arg2, arg3)
+  (arg1: A, arg2: B, arg3: C, ..._: any[]): R => fn(arg1, arg2, arg3)
 
 export const voidify = (fn: (...args: any[]) => any) =>
   (...args: any[]): void => { fn(...args) }
+
+export const spread = (fn: (...args: any[]) => any) =>
+  (args: any[]): any => fn(...args)
+
+export const gather = (fn: (args: any[]) => any) =>
+  (...args: any[]): any => fn(args)
+
+export const named = (...propNames: string[]) => (fn: (props: { [key: string]: any }) => any) =>
+  (...args: any[]): any => fn(propNames.reduce((res: { [key: string]: any }, name, i) => {
+    res[name] = args[i]
+    return res
+  }, {}))
+
+export const positional = (...propNames: string[]) => (fn: (...args: any[]) => any) =>
+  (props: { [key: string]: any }): any => fn(...propNames.map((name) => props[name]))
