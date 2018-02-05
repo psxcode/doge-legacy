6716177169d29ac1f8bdbcbe38809a0f5956c70f
alex.feinstein <alex.feinstein@klarna.com>
2018-02-05 18:54:07 +0100

    feat(stream): add debounce

---
+++ packages/stream/src/debounce.spec.ts
@@ -0,0 +1,30 @@
+import { iterate } from '@doge/helpers'
+import { makeWritable } from './helpers/writable'
+import { makeReadable } from './helpers/readable'
+import { makeSmallRange, makeTransformTest, xmakeTransformTest } from './helpers/helpers'
+import { debounceTime, debounce } from './debounce'
+
+describe('[ debounce ]', function () {
+  const interval = (next: () => void) => {
+    console.log('subscribe')
+    const id = setTimeout(next, 30)
+    return () => {
+      console.log('clear')
+      clearTimeout(id)
+    }
+  }
+
+  describe('[debounce]', function () {
+    xmakeTransformTest<number>(makeSmallRange(4),
+      (data) => makeReadable({ delayMs: 0 })({ objectMode: true })(iterate(data)),
+      (spy) => makeWritable({})({ objectMode: true })(spy),
+      () => debounce(interval))
+  })
+
+  describe('[debounceTime]', function () {
+    xmakeTransformTest<number>(makeSmallRange(4),
+      (data) => makeReadable({ delayMs: 0 })({ objectMode: true })(iterate(data)),
+      (spy) => makeWritable({})({ objectMode: true })(spy),
+      () => debounceTime(30))
+  })
+})
---
+++ packages/stream/src/debounce.ts
@@ -0,0 +1,32 @@
+import { Transform, TransformOptions } from 'stream'
+import { wait } from '@doge/helpers'
+
+export const debounceRaw = (opts: TransformOptions) =>
+  (wait: (cb: () => void) => () => void) => {
+    let lastChunk: any
+    let unsubscribe: any
+    return new Transform({
+      ...opts,
+      transform (chunk, encoding, callback) {
+        lastChunk = chunk
+        unsubscribe && unsubscribe()
+        unsubscribe = wait(() => {
+          unsubscribe = undefined
+          this.push(lastChunk)
+          lastChunk = undefined
+        })
+        callback()
+      },
+      flush (callback) {
+        unsubscribe && unsubscribe()
+        callback(null, lastChunk)
+      }
+    })
+  }
+
+export const debounce = debounceRaw({ objectMode: true })
+
+export const debounceTimeRaw = (opts: TransformOptions) =>
+  (ms: number) => debounceRaw(opts)(wait(setTimeout, clearTimeout)(ms))
+
+export const debounceTime = debounceTimeRaw({ objectMode: true })
