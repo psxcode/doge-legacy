2c63d2882d5b5cb6c14733b51553301746242511
alex.feinstein <alex.feinstein@klarna.com>
2018-02-19 17:12:09 +0100

    refactor(rx-node): from-stream use @doge/stream subscribe function

--- packages/rx-node/src/observable/from-stream.ts
+++ packages/rx-node/src/observable/from-stream.ts
@@ -1,33 +1,8 @@
 import ReadableStream = NodeJS.ReadableStream
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/share'
+import { subscribe } from '@doge/stream'
 
-export function fromStream<T> (source: ReadableStream): Observable<T> {
-  const subscribeToStream = (onData: (data?: T) => void,
-                             onError: (e: any) => void,
-                             onEnd: () => void) => {
-    source.on('data', onData)
-    source.on('end', onEnd)
-    source.on('error', onError)
-    return () => {
-      source.removeListener('data', onData)
-      source.removeListener('end', onEnd)
-      source.removeListener('error', onError)
-    }
-  }
-  return new Observable<T>((observer) => {
-    const unsub = subscribeToStream(
-      (chunk: T) => {
-        observer.next(chunk)
-      },
-      (e: any) => {
-        unsub && unsub()
-        observer.error(e)
-      },
-      () => {
-        unsub && unsub()
-        observer.complete()
-      })
-    return unsub
-  }).share()
+export function fromStream<T> (source: ReadableStream) {
+  return new Observable<T>(observer => subscribe(observer)(source)).share()
 }
