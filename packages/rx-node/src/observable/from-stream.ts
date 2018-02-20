e2061b2e090f95ac2d5e6d822b32cfd568591660
alex.feinstein <alex.feinstein@klarna.com>
2018-02-20 14:17:07 +0100

    fix(rx-node): fix from-stream binding

--- packages/rx-node/src/observable/from-stream.ts
+++ packages/rx-node/src/observable/from-stream.ts
@@ -1,8 +1,8 @@
 import ReadableStream = NodeJS.ReadableStream
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/share'
-import { subscribe } from '@doge/stream'
+import { subscribe, bindObserver } from '@doge/stream'
 
 export function fromStream<T> (source: ReadableStream) {
-  return new Observable<T>(observer => subscribe(observer)(source)).share()
+  return new Observable<T>(observer => subscribe(bindObserver(observer))(source)).share()
 }
