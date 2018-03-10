80dad763d37b56914be4b91e6732a01e9844332a
psxcode <psxcode@gmail.com>
2018-03-10 17:17:51 +0100

    refactor(helpers): remove throw-on

--- packages/helpers/src/throw-on.ts
+++
@@ -1,8 +0,0 @@
-export const throwOn = <T> (predicate: (arg: T) => boolean, throwFn: () => never) =>
-  (fn: (...args: any[]) => T) => (...args: any[]): T | never => {
-    const result: T = fn(...args)
-    if (!predicate(result)) {
-      throwFn()
-    }
-    return result
-  }
