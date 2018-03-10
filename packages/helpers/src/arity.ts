89f791c2f74ebd1da319b05274c12d9ceef6bafe
psxcode <psxcode@gmail.com>
2018-03-10 22:27:16 +0100

    refactor(helpers): refactor branch

--- packages/helpers/src/arity.ts
+++ packages/helpers/src/arity.ts
@@ -64,5 +64,5 @@ export const constant = <T> (arg: T) => () => arg
 
 export const constantAsync = <T> (arg: T) => () => Promise.resolve(arg)
 
-export const branch = <T> (pred: (arg: T) => boolean, fn0: AnyFn, fn1: AnyFn) =>
-  (arg: T) => pred(arg) ? fn0(arg) : fn1(arg)
+export const branch = <T> (pred: (arg?: T) => boolean, fn0: AnyFn, fn1: AnyFn) =>
+  (arg?: T) => pred(arg) ? fn0(arg) : fn1(arg)
