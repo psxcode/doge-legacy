f1422ca590c2c91e521b788dd2f974f84fa8d55a
psxcode <psxcode@gmail.com>
2018-03-26 02:59:49 +0200

    refactor(iterable): simplefy resolve code

--- packages/iterable/src/resolve.ts
+++ packages/iterable/src/resolve.ts
@@ -1,9 +1,8 @@
 export const resolve = (iter: Iterator<any>) => {
-  const handle = (ir: IteratorResult<any>) => {
+  const handle = async (ir: IteratorResult<any>) => {
     if (!ir.done) {
-      Promise.resolve(ir.value)
-        .then(val => handle(iter.next(val)))
+      handle(iter.next(await ir.value))
     }
   }
-  handle(iter.next())
+  return handle(iter.next())
 }
