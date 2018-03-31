d24e2686d249e3b307fc5b6f6ba5a053e1c3db0a
psxcode <psxcode@gmail.com>
2018-03-31 23:57:49 +0200

    refactor(bittrex-api): use compose for ho-funcs

--- packages/bittrex-api/src/request-credential.ts
+++ packages/bittrex-api/src/request-credential.ts
@@ -1,5 +1,5 @@
 import { Request, RequestInit, Response } from 'node-fetch'
-import { pipe } from '@doge/helpers'
+import { compose } from '@doge/helpers'
 import { fetchGet, fetchApikey } from '@doge/fetch'
 import { nonce } from './nonce'
 import { fetchApisign } from '../../fetch/src'
@@ -7,4 +7,4 @@ import { hash } from './hash'
 import { ICredentialApiInit } from './types'
 
 export const request = (opts: ICredentialApiInit) =>
-  pipe(fetchGet, fetchApikey(opts.apikey, nonce), fetchApisign(hash(opts.apisecret)))
+  compose(fetchGet, fetchApikey(opts.apikey, nonce), fetchApisign(hash(opts.apisecret)))
