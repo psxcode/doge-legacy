884d13a1c694fa292eef27beafdb0e9d0220adf1
psxcode <psxcode@gmail.com>
2018-07-02 00:11:12 +0200

    refactor(arity): refactor bind

--- packages/arity/src/bind.spec.ts
+++ packages/arity/src/bind.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { pipe } from '@psxcode/compose'
 import bind from './bind'
 
-const id = (value: any) => value
+const id = (value: number) => value
 const add = (a: number, b: number) => a + b
 const addNamed = (a: string, b: string) => (p: {[k: string]: any}) => p[a] + p[b]
 const sum = (...args: number[]) => args.reduce(add)
@@ -12,17 +12,17 @@ const getCtx = () => {
 
 describe('[ bind ]', () => {
   it('should work as a constant', () => {
-    const binded = bind(10)(id)
+    const binded = bind(id, 10)
     expect(binded()).eq(10)
   })
 
   it('should work', () => {
-    const binded = bind(10)(add)
+    const binded = bind(add, 10)
     expect(binded(10)).eq(20)
   })
 
-  it('should work with pipe', () => {
-    const binded = pipe(bind(10, 20), bind(10), bind(2))(sum)
-    expect(binded()).eq(42)
-  })
+  // it('should work with pipe', () => {
+  //   const binded = pipe(bind(10, 20), bind(10), bind(2))(sum)
+  //   expect(binded()).eq(42)
+  // })
 })
