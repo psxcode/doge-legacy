f3f87ad2c571ce0c952bf52c143daae3ae47afe4
alex.feinstein <alex.feinstein@klarna.com>
2018-07-03 17:50:59 +0200

    chore(arity): refactor tests

--- packages/arity/src/and.spec.ts
+++ packages/arity/src/and.spec.ts
@@ -3,17 +3,17 @@ import and from './and'
 
 describe('[ and ]', () => {
   it('should work', async () => {
-    const pred = and(x => x === 1, x => x !== 1)
+    const pred = and((x: number) => x === 1, (x: number) => x !== 1)
     expect(pred(1)).eq(false)
   })
 
   it('should work', async () => {
-    const pred = and(x => x === 1, () => true)
+    const pred = and((x: number) => x === 1, () => true)
     expect(pred(1)).eq(true)
   })
 
   it('should work', async () => {
-    const pred = and(x => x === 1, x => !!x && x > 0)
+    const pred = and((x: number) => x === 1, (x: number) => !!x && x > 0)
     expect(pred(1)).eq(true)
   })
 })
--- packages/arity/src/or.spec.ts
+++ packages/arity/src/or.spec.ts
@@ -3,17 +3,17 @@ import or from './or'
 
 describe('[ or ]', () => {
   it('should work', async () => {
-    const pred = or(x => x === 1, x => x !== 1)
+    const pred = or((x: number) => x === 1, x => x !== 1)
     expect(pred(1)).eq(true)
   })
 
   it('should work', async () => {
-    const pred = or(x => x === 1, () => false)
+    const pred = or((x: number) => x === 1, () => false)
     expect(pred(0)).eq(false)
   })
 
   it('should work', async () => {
-    const pred = or(x => x === 1, x => !!x && x > 0)
+    const pred = or((x: number) => x === 1, x => !!x && x > 0)
     expect(pred(5)).eq(true)
   })
 })
