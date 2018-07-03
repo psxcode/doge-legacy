4a788b068767cc6e02df7be2364fc6d45a5331cd
alex.feinstein <alex.feinstein@klarna.com>
2018-07-03 17:51:55 +0200

    fix(arity): export tuple, remove types

--- packages/arity/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type IHash <T> = {[key: string]: T}
-export type NamedFn <R> = (props: IHash<any>) => R
-export type PositionalFn <R> = (...args: any[]) => R
-export type AnyFn = (...args: any[]) => any
